f03234358c5460c8c67302c2b8c47bf015f9afa7
Arlen Emir Ertunç <92711414+ArlenEmir@users.noreply.github.com>
2022-03-11 00:08:33 +0300

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -9,6 +9,10 @@
 # print(Toplama(n))
 
 
+
+
+
+
 # Kenar1 = int(input('1. Kenar Uzunluğunu Giriniz : '))
 # Kenar2 = int(input('2. Kenar Uzunluğunu Giriniz : '))
 #
@@ -17,14 +21,17 @@
 # Alan = Kenar1*Kenar2
 # print("Dikdörtgenin alanı: ",Alan)
 
-import math
 
-SilindirinYüksekligi = int(input('Silindirin Yüksekliğini Giriniz: '))
-SilindirinYariCapi = int(input('Silindirin Yari Capini Giriniz: '))
-def SilindirinHacminiHesaplayanDef():
-    return (2*math.pi)*(SilindirinYariCapi^2)+2*(math.pi*SilindirinYariCapi)*SilindirinYüksekligi
 
-Alan = (2*math.pi)*(SilindirinYariCapi^2)+2*(math.pi*SilindirinYariCapi)*SilindirinYüksekligi
-print("Silindirin Hacmi: ", Alan)
 
-Alan = 2πr(r+h)
+
+#import math
+
+#SilindirinYüksekligi = int(input('Silindirin Yüksekliğini Giriniz: '))
+#SilindirinYariCapi = int(input('Silindirin Yari Capini Giriniz: '))
+#def SilindirinHacminiHesaplayanDef():
+#    return (math.pi*SilindirinYariCapi^2)*(SilindirinYüksekligi+(math.pi*SilindirinYariCapi^2)/2)
+
+#Alan = (math.pi*SilindirinYariCapi^2)*(SilindirinYüksekligi+(math.pi*SilindirinYariCapi^2)/2)
+#print("Silindirin Hacmi: ", Alan)
+
