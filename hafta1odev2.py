cf6da0f513efd0c6ce34396a84ed500e169bd954
Arlen Emir Ertunç <92711414+ArlenEmir@users.noreply.github.com>
2022-03-29 14:26:02 +0300

    Update hafta1odev2.py

--- hafta1odev2.py
+++ hafta1odev2.py
@@ -1,25 +1,18 @@
-def Toplama(n):
-    toplam = 0
-    for rakam in str(n):
-        toplam += int(rakam)
-    return toplam
-
-sayi1 = int(input('4 Basamaklı Sayıyı Giriniz : '))
-n = sayi1
-print(Toplama(n))
-
-
-Kenar1 = int(input('1. Kenar Uzunluğunu Giriniz : '))
-Kenar2 = int(input('2. Kenar Uzunluğunu Giriniz : '))
-
-def DikdörtgenAlanHesaplamaDefi():
-    return  Kenar1*Kenar2
-Alan = Kenar1*Kenar2
-print("Dikdörtgenin alanı: ",Alan)
-
 import math
+kenar1 = float(input("1. kenarı giriniz: "))
+kenar2 = float(input("2. kenarı giriniz: "))
 
-SilindirinYüksekligi = int(input('Silindirin Yüksekliğini Giriniz: '))
-SilindirinYariCapi = int(input('Silindirin Yari Capini Giriniz: '))
-def SilindirinHacminiHesaplayanDef():
-    return (2*math.pi)*(SilindirinYariCapi^2)+2*(math.pi*SilindirinYariCapi)*SilindirinYüksekligi
+diktörtgenalani= kenar1*kenar2
+
+print(diktörtgenalani)
+
+
+#################################################################################################
+
+
+yaricap = float(input("yari çap giriniz: "))
+yukseklik = float(input("yüksekliği giriniz: "))
+
+silindirinalani = (2*math.pi*(yaricap**2)+(2*math.pi*yukseklik*yaricap))
+
+print(silindirinalani)
